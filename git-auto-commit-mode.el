18f9fb41e47da304ec2646d7895d0b01de75b743
Tom Willemsen <tom@ryuslash.org>
2012-05-20 14:15:46 +0200

    Fix docs
    Fix tiny typo and filling in docs.

--- git-auto-commit-mode.el
+++ git-auto-commit-mode.el
@@ -45,7 +45,7 @@
     relative-file-name))
 
 (defun git-auto-commit ()
-  "Commit `buffer-file-name` to git"
+  "Commit `buffer-file-name' to git"
   (let* ((filename (buffer-file-name))
          (relative-filename
           (git-auto-commit-relative-file-name filename)))
@@ -54,8 +54,8 @@
              " && git commit -m '" relative-filename "'"))))
 
 (define-minor-mode git-auto-commit-mode
-  "Automatically commit any changes made when saving with this mode
-turned on"
+  "Automatically commit any changes made when saving with this
+mode turned on"
   :lighter " ga"
   (if git-auto-commit-mode
       (add-hook 'after-save-hook 'git-auto-commit t t)
