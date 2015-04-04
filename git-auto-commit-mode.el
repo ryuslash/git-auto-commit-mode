075e5f9ded66c2035581a7b216896556cc586814
Tom Willemse <tom@ryuslash.org>
2015-04-04 16:51:09 +0200

    Bump version

--- git-auto-commit-mode.el
+++ git-auto-commit-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Willemse <tom@ryuslash.org>
 ;; Created: Jan 9, 2012
-;; Version: 4.3.2
+;; Version: 4.4.0
 ;; Keywords: vc
 ;; URL: http://projects.ryuslash.org/git-auto-commit-mode/
 
